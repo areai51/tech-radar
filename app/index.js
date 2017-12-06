f0844f383d9166fa24159d343000e9ecb38ace77
DESKTOP-1OH2S1U\Ranjan <rsi159@sapient.com>
2017-12-06 10:16:19 +0530

    Separated config file from main app.

--- app/index.js
+++ app/index.js
@@ -1,53 +1,12 @@
-const config={
-  "svg_id": "radar",
-	"width": 1450,
-	"height": 1000,
-	"colors": {
-		"background": "#fff",
-		"grid": "#bbb",
-		"inactive": "#ddd"
-	},
-	"title": "Sapient Razorfish Tech Radar",
-	"quadrants": [{
-			"name": "Languages"
-		},
-		{
-			"name": "Infrastructure"
-		},
-		{
-			"name": "Frameworks"
-		},
-		{
-			"name": "Data Management"
-		}
-	],
-	"rings": [{
-			"name": "ADOPT",
-			"color": "#93c47d"
-		},
-		{
-			"name": "TRIAL",
-			"color": "#93d2c2"
-		},
-		{
-			"name": "ASSESS",
-			"color": "#fbdb84"
-		},
-		{
-			"name": "HOLD",
-			"color": "#efafa9"
-		}
-	],
-	"print_layout": true,
-	// zoomed_quadrant: 0,
-	"entries": [
-		{
-			"quadrant": 3,
-			"ring": 0,
-			"label": "AWS EMR",
-			"active": false,
-    }
-  ]
 
-}
-radar_visualization(config);
+$(document).ready(function(){
+	$.getJSON( "data")
+	.done(function( config ) {
+	  radar_visualization( config );
+	})
+	.fail(function( jqxhr, textStatus, error ) {
+	  var err = textStatus + ", " + error;
+	  console.log( "Request Failed: " + err );
+  });
+})
+
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express'),
       path=require('path'),
-app = express();
+      fs = require("fs"),
+      app = express();
 app.set('port', 3000);
 app.use(express.static(path.join(__dirname, 'app')));
 const server = app.listen(app.get('port'), function() {
 console.log('Server running on port ' + 3000);
 });
+
+app.get('/data', function (req, res) {
+      res.sendFile(__dirname+'/data/radar-config.json');
+})
